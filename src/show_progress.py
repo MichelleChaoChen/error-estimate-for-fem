15121a6f3ec50f6b51075dc924faca8b2cca67c6
Manh Tan Nguyen <nguyenmanhtan@student.tudelft.nl>
2022-12-01 13:20:37 +0100

    refactored training code and try to train old feature model with 100 epochs - loss 0.5 MSE

--- src/show_progress.py
+++
@@ -1,5 +0,0 @@
-from tensorflow import keras
-
-class Show_Progress(keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs):
-        print("Epoch = {} Loss = {} Validation Loss = {}".format(epoch, logs["loss"], logs["val_loss"]))
--- src/train.py
+++ src/train.py
@@ -4,16 +4,30 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from show_progress import Show_Progress
 
 class NeuralNetwork: 
-    def __init__(self, train_data):
-        self.train_labels = np.array(train_data.pop(train_data.shape[1] - 1))
-        self.train_features = np.array(train_data)
+    def __init__(self, dataset, epochs):
+        train_data, test_data = self.split_data(dataset, 0.2) # 0.8 for training, 0.2 for testing
+        self.train_features, self.train_labels = self.get_attr(train_data)
+        self.test_features, self.test_labels = self.get_attr(test_data)
 
-        self.train()
-    
-    def train(self):
+        print("START TRAINING")
+        self.train(epochs)
+
+        print("START EVALUATING")
+        self.evaluate()
+
+    def get_attr(self, data):
+        labels = np.array(data.pop(data.shape[1] - 1))
+        features = np.array(data)
+
+        return features, labels
+
+    def split_data(self, dataset, split_ratio):
+        test_indices = np.random.rand(len(dataset)) < split_ratio
+        return dataset[~test_indices], dataset[test_indices]
+
+    def train(self, epochs=100):
         feature_normalizer = layers.Normalization(
             input_shape=np.array(self.train_features[0]).shape, 
             axis=-1
@@ -28,27 +42,28 @@ class NeuralNetwork:
             layers.Dense(1)
         ])
 
-        self.model.compile(loss='mean_absolute_error', 
+        self.model.compile(loss='mean_squared_error', 
             optimizer=tf.keras.optimizers.Adam(0.001))
 
         # train the model 
         self.model.fit(self.train_features, 
             self.train_labels, 
-            epochs=100, 
-            verbose=0, 
-            validation_split=0.2,
-            callbacks=[
-                Show_Progress(),
-            ]
+            epochs=epochs, 
+            verbose=2, 
         )
 
+    def evaluate(self):
+        score = self.model.evaluate(self.test_features, self.test_labels, verbose=2)
+        print("EVALUATED MSE {}".format(score))
+
     def save(self, name):
-        self.model.save("models/"+name+".ckpt")
+        if self.model is not None:
+            self.model.save("models/"+name+".ckpt")
 
 def main(args):
-    data = pd.read_csv(args.data, sep=" ", header=None)
+    data = pd.read_csv(args.data, sep=",", header=None)
 
-    error_estimate_model = NeuralNetwork(data)
+    error_estimate_model = NeuralNetwork(data, int(args.epochs))
     error_estimate_model.save(args.data.split("/")[-1].split(".")[0])
 
 if __name__ == '__main__':
@@ -58,6 +73,13 @@ if __name__ == '__main__':
                         '--data',
                         required=True,
                         help="path to training data")
+
+    parser.add_argument('-epochs',
+                        '--epochs',
+                        required=False,
+                        default=100,
+                        help="number of epochs for training")
+
 
     args = parser.parse_args()
     main(args)
