01a9902c9ff328ce7ddd682d9828be7b11da326e
abaillet <bailletalvaro4@gmail.com>
2022-12-20 19:12:48 +0100

    added features function

--- src/adaptive_mesh_refinement.py
+++ src/adaptive_mesh_refinement.py
@@ -8,6 +8,51 @@ def get_error_estimate(features):
     global_error = np.linalg.norm(local_errors)
     return local_errors, global_error
 
+##########################################
+# Feature generation
+##########################################
+def sampleSource(func, xgrid, frac):
+    hs = xgrid[1:] - xgrid[:-1]
+    x_sample_source = np.tile(frac, (len(xgrid),1))
+    hs_copy = np.append(hs, 1)
+    x_copy = xgrid.copy()
+    x_copy = np.reshape(x_copy, (len(xgrid), 1))
+    
+    x_sample_source = x_sample_source * hs_copy[:, np.newaxis]
+    x_sample_source = x_sample_source + x_copy
+    x_sample_source = x_sample_source.flatten()
+    
+    f_samples = func(x_sample_source)
+    f_samples = np.reshape(f_samples, (len(xgrid), len(frac)))[:-1]
+
+
+    f_im1 = f_samples[0:-2]
+    f_i = f_samples[1:-1]
+    f_ip1 = f_samples[2:]
+    
+    func_sample = np.hstack((f_im1, f_i, f_ip1))
+    return func_sample
+
+def gradient(u_fem, xgrid):
+    grad_fem = (u_fem[1:] - u_fem[:-1])/(xgrid[1:] - xgrid[:-1]) 
+    grad_im1 = grad_fem[0:-2]
+    grad_i = grad_fem[1:-1]
+    grad_ip1 = grad_fem[2:]
+    
+    grad_sample = np.vstack((grad_im1, grad_i, grad_ip1))
+    grad_sample = np.transpose(grad_sample)
+    return grad_sample
+
+def get_features(func, xgrid, u_fem, frac=[0.25, 0.5, 0.75]):  
+    hs = xgrid[1:] - xgrid[:-1]
+    hs = hs.reshape(-1, 1)
+    hs = hs[1:-1]
+    f_sample = sampleSource(func, xgrid, frac)
+    g_sample = gradient(u_fem, xgrid)
+    data = np.hstack((f_sample, g_sample, hs))
+    return data
+
+
 
 def adaptive_mesh_refinement():
     mesh = np.linspace(0, 1, 9)
